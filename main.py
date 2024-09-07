56f5af585c403f5e9411dfb9c23dc22a6f91f646
Tsuquon <liamling04@gmail.com>
2024-09-07 10:06:35 +1000

    pre frontend

--- main.py
+++ main.py
@@ -18,18 +18,6 @@ def main():
     interface = TerminalInterface()
     interface.run_program()
     
-        
-    # word_bank = interface.input_interface()
-    # llm_prompt(word_bank)
-    # random.shuffle(word_bank)
-    # for word in word_bank:
-        # user_input = input(f"Enter the translation for {word[0]}: ")
-    #     # print(word)
-    #     llm_prompt(word, user_input)
-    
-    # df = clean_df()
-    # insert_into_database(df)
-
 
 if __name__ == "__main__":
     main()
